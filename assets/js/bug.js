ccde2a1a7c70859c1c311e9d8df4ca64b7ea9a74
juraj.gunis <jgunis@gmail.com>
2016-11-14 15:55:15 +0100

    Remaking bug to be displayed via `fa icons` with using jQuery plugin `hoverIntent`.

--- assets/js/bug.js
+++ assets/js/bug.js
@@ -9,23 +9,36 @@ $(function ($) {
 		return [y, x];
 	}
 	var startpos = getNewPosition();
-	$('#bug').css({
-		top : startpos[0],
-		left : startpos[1]
-	});
-	$('#bug').hover(function() {
-		var pos = getNewPosition();
-		var rotate = Math.floor(Math.random() * 360);
-		$(this).css({
-			'-webkit-transform' : 'rotate('+rotate+'deg)',
-			'-moz-transform' : 'rotate('+rotate+'deg)',
-			'-o-transform' : 'rotate('+rotate+'deg)',
-			'-ms-transform' : 'rotate('+rotate+'deg)',
-			'transform' : 'rotate('+rotate+'deg)'
+	$('#bug')
+		.css({
+			top : startpos[0],
+			left : startpos[1]
 		});
-		$(this).animate({
-			top : pos[0],
-			left : pos[1]
-		}, 150);
-	}, function() {});
+	$('#bug')
+		.hoverIntent(function() {
+			var pos = getNewPosition();
+			var rotate = Math.floor(Math.random() * 360);
+			$(this)
+				.css({
+					'-webkit-transform' : 'rotate('+rotate+'deg)',
+					'-moz-transform' : 'rotate('+rotate+'deg)',
+					'-o-transform' : 'rotate('+rotate+'deg)',
+					'-ms-transform' : 'rotate('+rotate+'deg)',
+					'transform' : 'rotate('+rotate+'deg)',
+					'opacity' : '.7'
+				});
+			$(this)
+				.animate(
+					{
+						top : pos[0],
+						left : pos[1]
+					},
+					150,
+					function(){
+						$(this)
+							.css({
+								'opacity' : '1'
+							});
+					});
+		}, function() {});
 });
